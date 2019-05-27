ca3df30b472b83bb702d63eda6868de7a41ecda9
LRLVEC <mjdyx@outlook.com>
2019-05-27 17:58:58 +0800

    a

--- GL/_OptiX.h
+++ GL/_OptiX.h
@@ -11,7 +11,7 @@ namespace OpenGL
 {
 	namespace OptiX
 	{
-		struct DefautRenderer :Program
+		struct DefautRenderer : ::OpenGL::Program
 		{
 			struct TriangleData :Buffer::Data
 			{
@@ -30,10 +30,10 @@ namespace OpenGL
 				}
 				virtual unsigned int size()override
 				{
-					return sizeof(Triangle)* triangles.length;
+					return sizeof(Triangle) * triangles.length;
 				}
 			};
-			struct PixelData :Buffer::Data
+			struct PixelData : ::OpenGL::Buffer::Data
 			{
 				Texture frameTexture;
 				TextureConfig<TextureStorage2D> frameConfig;
@@ -51,7 +51,7 @@ namespace OpenGL
 				}
 				virtual unsigned int size()override
 				{
-					return frameConfig.width* frameConfig.height * 16;
+					return frameConfig.width * frameConfig.height * 16;
 				}
 			};
 
@@ -122,7 +122,9 @@ namespace OpenGL
 				Vector<String<char>>functions;
 				bool operator==(String<char> const& _name)const
 				{
-					return name == _name;
+					for (int c0(0); c0 < functions.length; ++c0)
+						if (_name == functions.data[c0])
+							return true;
 				}
 			};
 			File* folder;
@@ -132,69 +134,165 @@ namespace OpenGL
 				:
 				folder(_folder)
 			{
-				getSource();
+				readSource();
 			}
-			void getSource(Vector<String<char>>const& _names)
+			void readSource()
 			{
 				File& ptx(folder->find("ptx"));
 				String<char>PTXLists(ptx.find("PTXLists.txt").readText());
-				Vector<int>functions(PTXLists.find("Program:"));
-				for (int c0(0); c0 < functions.length; ++c0)
+				Vector<int>files(PTXLists.find("Program:"));
+				for (int c0(0); c0 < files.length; ++c0)
 				{
-					int n(functions[c0]);
+					int n(files[c0]);
 					int delta;
 					char t[100];
-					n += sweepStr(PTXLists.data + n, "Program:%n");
+					char t1[10];
+					n += sweepStr(PTXLists.data + n, "Program:%*[^a-zA-Z0-9_]%n");
 					sscanf(PTXLists.data + n, "%[a-zA-Z0-9_]%n", t, &delta);
 					n += delta;
 					Source s;
 					s.name = t;
-					s.source = ptx.findInThis(s.name + ".cu").readText();
-
-				}
-				for (int c0(0); c0 < _names.length; ++c0)
-				{
-					String<char>s(ptx.findInThis(_names.data[c0] + ".ptx").readText());
-					if (s.length)sources.pushBack({ _names.data[c0],s });
+					s.source = ptx.findInThis(s.name + ".ptx").readText();
+					n += sweepStr(PTXLists.data + n, "%*[^{]%{%n");
+					int j(0);
+					do
+					{
+						j = sscanf(PTXLists.data + n, "%s%n%*[\t\r\n ]%[}]", t, &delta, t1);
+						n += delta;
+						if (j < 1)break;
+						s.functions.pushBack(t);
+					}
+					while (j == 1);
+					sources.pushBack(s);
 				}
 			}
-			String<char>const& getFunction(String<char>_name)const
+			String<char>& getFunction(String<char>const& _name)const
 			{
 				for (int c0(0); c0 < sources.length; ++c0)
 					if (sources.data[c0] == _name)
 						return sources.data[c0].source;
 			}
 		};
-		struct Function
+		struct Buffer
+		{
+			RTcontext* context;
+			RTbuffer  buffer;
+			RTbuffertype type;
+			RTformat format;
+
+			Buffer(RTcontext* _context, RTbuffertype _type, RTformat _format, GLuint _pbo)
+				:
+				context(_context),
+				type(_type),
+				format(_format)
+			{
+				create(_pbo);
+			}
+			void create(GLuint _pbo)
+			{
+				rtBufferCreateFromGLBO(*context, type, _pbo, &buffer);
+			}
+			void setFormat()
+			{
+				rtBufferSetFormat(buffer, format);
+			}
+			void setElementSize(unsigned int _size)
+			{
+				if (type == RT_FORMAT_USER)
+					rtBufferSetElementSize(buffer, _size);
+			}
+			void setSize(unsigned int _width)
+			{
+				rtBufferSetSize1D(buffer, _width);
+			}
+			void setSize(unsigned int _width, unsigned int _height)
+			{
+				rtBufferSetSize2D(buffer, _width, _height);
+			}
+			void setSize(unsigned int _width, unsigned int _height, unsigned int _depth)
+			{
+				rtBufferSetSize3D(buffer, _width, _height, _height);
+			}
+
+		};
+		struct Variable
+		{
+			RTvariable variable;
+
+		};
+		struct Program
 		{
 			RTcontext* context;
 			RTprogram program;
-			String<char>source;
+			String<char>* source;
 			String<char>name;
 
-			Function() = delete;
-			Function(RTcontext* _context, String<char>const& _source, String<char>_name)
+			Program() = delete;
+			Program(RTcontext* _context, PTXManager const& _pm, String<char>_name)
 				:
 				context(_context),
-				source(_source),
+				source(&_pm.getFunction(_name)),
 				name(_name)
 			{
 				create();
 			}
 			void create()
 			{
-				rtProgramCreateFromPTXString(*context, source, name, &program);
+				rtProgramCreateFromPTXString(*context, *source, name, &program);
 			}
 			operator RTprogram()const
 			{
 				return program;
 			}
 		};
+		struct Context
+		{
+			RTcontext context;
+			Vector<Program*>entryPoints;
+			unsigned int rayTypeCount;
+
+			Context() = delete;
+			Context(Vector<Program*>const& _entryPoints, unsigned int _rayTypeCount)
+				:
+				context(0),
+				entryPoints(_entryPoints),
+				rayTypeCount(_rayTypeCount)
+			{
+				create();
+			}
+			operator RTcontext()const
+			{
+				return context;
+			}
+			void create()
+			{
+				rtContextCreate(&context);
+			}
+			void init()
+			{
+				rtContextSetRayTypeCount(context, rayTypeCount);
+				rtContextSetEntryPointCount(context, entryPoints.length);
+				for (int c0(0); c0 < entryPoints.length; ++c0)
+					rtContextSetRayGenerationProgram(context, c0, *entryPoints[c0]);
+			}
+			void validate()
+			{
+				rtContextValidate(context);
+			}
+			void launch(unsigned int _entryPoint, unsigned int x)
+			{
+				rtContextLaunch1D(context, _entryPoint, x);
+			}
+			void destory()
+			{
+				rtContextDestroy(context);
+			}
+		};
 
 		struct RayTracer
 		{
 			RTcontext context;
-			Function* rayGen;
+			Program* rayGen;
 			RTbuffer  outBuffer;
 
 		};
