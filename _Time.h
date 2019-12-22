14ba1a4760e0c736f290dda0672eff0ca37b241a
LRLVEC <mjdyx@outlook.com>
2019-12-23 01:57:54 +0800

    add Timer::print(ull)

--- _Time.h
+++ _Time.h
@@ -13,6 +13,7 @@ struct Timer
 	void end();
 	void print();
 	void print(const char* a);
+	void print(long long minus);
 };
 struct FPS
 {
@@ -68,6 +69,25 @@ inline void Timer::print(const char* a)
 	printf(a);
 	print();
 }
+inline void Timer::print(long long minus)
+{
+	constexpr unsigned long long e3{ 1000U };
+	constexpr unsigned long long e6{ 1000000U };
+	constexpr unsigned long long e9{ 1000000000U };
+	constexpr unsigned long long e12{ 100000000000U };
+	long long dt = e9 * (ending.tv_sec - begining.tv_sec) + (ending.tv_nsec - begining.tv_nsec) - minus;
+	if (dt < 0)printf("-");
+	unsigned long long dt1 = llabs(dt);
+	if (dt1 >= 500)dt1 -= 500;
+	else dt1 = 0;
+	double length = log10(dt1);
+	if (length < 3)printf("%15llu ns", dt1);
+	else if (length < 6)printf("%16llu,%03llu ns", dt1 / e3, dt1 % e3);
+	else if (length < 9)printf("%12llu,%03llu,%03llu ns", dt1 / e6, (dt1 % e6) / e3, dt1 % e3);
+	else if (length < 12)printf("%8llu,%03llu,%03llu,%03llu ns", dt1 / e9, (dt1 % e9) / e6, (dt1 % e6) / e3, dt1 % e3);
+	else if (length < 15)printf("%4llu,%03llu,%03llu,%03llu,%03llu ns", dt1 / e12, (dt1 % e12) / e9, (dt1 % e6) / e6, (dt1 % e6) / e6, dt1 % e3);
+	printf("\n");
+}
 //==================FPS==================
 inline FPS::FPS()
 	:
@@ -104,7 +124,7 @@ inline void FPS::printFrameTime(unsigned int a)
 }
 inline void FPS::printFrameTimeToString(unsigned int a)
 {
-	if (valid)sprintf(str,"frame time: %.*f ms", a, dt / 100000.0);
+	if (valid)sprintf(str, "frame time: %.*f ms", a, dt / 100000.0);
 }
 inline void FPS::printFPSAndFrameTime(unsigned int a, unsigned int b)
 {
